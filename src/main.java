c50f0907c17151f9352587cb701e8b7c340f7eaf
MUSTAFA SAV <mustafasav@gmail.com>
2022-11-08 22:29:43 +0300

    8.11.2022

--- src/main.java
+++ src/main.java
@@ -3,7 +3,51 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		
+				String ortaMetin = "İlginizi Çekebilir";
+				String altMetin = "Vade Süresi";
+				
+				System.out.println(ortaMetin);
+			
+				
+				int vade = 12;
+				
+				
+				double dolarDun = 18.14;
+				double dolarBugun = 18.10;
+				
+				
+				boolean dolarDustuMu = false;
+				
+				
+				String okYonu = "";
+				
+				
+				if(dolarBugun < dolarDun) 
+				{
+					okYonu = "down.svg";
+					
+				}
+				else if(dolarBugun > dolarDun) 
+				{
+					okYonu = "up.svg";
+				}
+				else 
+				{
+					okYonu = "equal.svg";
+				}
+				
+				
+				System.out.println(okYonu);
+				
+				
+				String[] krediler = {"Hızlı Kredi", "Kamu Kredisi", "Mutlu Emekli"};
+				
+				
+				for (int i = 0; i < krediler.length; i++) 
+				{
+					System.out.println(krediler[i]);
+				}
 
 	}
 
